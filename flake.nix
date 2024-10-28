04dcf5d8c8dfa6bcb1c14ed1e2ae9aed108c0e0f
Jacob Koziej <jacobkoziej@gmail.com>
2024-10-27 20:56:37 -0400

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "The Cooper Union - ECE 418: Digital Video Processing";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    inputs:
+
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+
+        python = pkgs.python3;
+
+        python-pkgs = python.withPackages (
+          python-pkgs: with python-pkgs; [
+            ffmpeg-python
+            ipython
+          ]
+        );
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages =
+            [
+              python-pkgs
+            ]
+            ++ (with pkgs; [
+              black
+              ruff
+              treefmt2
+            ]);
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
