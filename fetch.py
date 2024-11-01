a16c497b78ba5fd63d81da81299102f2d13f133d
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-01 02:41:34 -0400

    Define `frames_external_format()`

--- fetch.py
+++ fetch.py
@@ -3,6 +3,10 @@
 # fetch.py -- fetch video
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
 
+import ffmpeg
+import numpy as np
+
+from collections.abc import Iterator
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -14,12 +18,47 @@ class VideoMetadata:
     width: int
 
 
+def frames_external_format(
+    path: Path,
+    metadata: VideoMetadata,
+) -> Iterator[np.ndarray]:
+    from subprocess import (
+        DEVNULL,
+        PIPE,
+        Popen,
+    )
+
+    args: list[str] = (
+        ffmpeg.input(path)
+        .output("pipe:", format="rawvideo", pix_fmt="gray")
+        .compile()
+    )
+
+    process: Popen = Popen(args, stdout=PIPE, stderr=DEVNULL)
+
+    frame_shape: tuple[int, int] = (metadata.height, metadata.width)
+    frame_bytes: int = metadata.height * metadata.width
+
+    while True:
+        in_bytes = process.stdout.read(frame_bytes)
+
+        if not len(in_bytes):
+            break
+
+        assert len(in_bytes) == frame_bytes
+
+        frame = np.frombuffer(in_bytes, np.uint8).reshape(frame_shape)
+
+        yield frame
+
+    process.stdout.close()
+    process.wait()
+
+
 def probe_external_format(
     path: Path,
     ffmpeg_args: dict[str, str] | None = None,
 ) -> VideoMetadata:
-    import ffmpeg
-
     from fractions import Fraction
 
     if ffmpeg_args is None:
