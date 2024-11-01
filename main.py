d2707f342ff3dd75ae3ecf71c871ea55b992e198
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-01 00:27:53 -0400

    Add `main.py`

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# main.py -- video compressor
+# Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
