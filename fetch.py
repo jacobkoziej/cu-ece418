619cd367e49650d5930f45ee4e5128923ea53330
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-01 01:32:06 -0400

    Define `VideoMetadata`

--- fetch.py
+++ fetch.py
@@ -2,3 +2,13 @@
 #
 # fetch.py -- fetch video
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class VideoMetadata:
+    frame_rate: float
+    height: int
+    width: int
