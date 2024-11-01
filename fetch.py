85052c4c47ad06792c28b21269384a2924b69952
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-01 01:47:37 -0400

    Define `probe_external_format()`

--- fetch.py
+++ fetch.py
@@ -12,3 +12,29 @@ class VideoMetadata:
     frame_rate: float
     height: int
     width: int
+
+
+def probe_external_format(
+    path: Path,
+    ffmpeg_args: dict[str, str] | None = None,
+) -> VideoMetadata:
+    import ffmpeg
+
+    from fractions import Fraction
+
+    if ffmpeg_args is None:
+        ffmpeg_args = {}
+
+    probe: dict[str, dict] = ffmpeg.probe(path, **ffmpeg_args)
+
+    stream: dict = next(
+        s for s in probe["streams"] if s["codec_type"] == "video"
+    )
+
+    frame_rate: float = float(Fraction(stream["r_frame_rate"]))
+
+    return VideoMetadata(
+        frame_rate=frame_rate,
+        height=stream["height"],
+        width=stream["width"],
+    )
