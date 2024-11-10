da5d6451194dbe25bf6932c7ce6470693701465d
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-09 23:41:32 -0500

    Define `EncoderConfig`

--- encode.py
+++ encode.py
@@ -2,3 +2,12 @@
 #
 # encode.py -- encode video
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+
+from dataclasses import dataclass
+
+
+@dataclass
+class EncoderConfig:
+    block_size: int = 16
+    height: int
+    width: int
