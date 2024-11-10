2d21b8466ef4f07c07131c35ebaf0e98a10481ef
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-10 12:31:42 -0500

    Define `IFrame`

--- frame.py
+++ frame.py
@@ -2,3 +2,12 @@
 #
 # frame.py -- frame formats
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+
+import numpy as np
+
+from dataclasses import dataclass
+
+
+@dataclass
+class IFrame:
+    frame: np.ndarray
