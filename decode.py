e7aed8c09b433e630a3248ac396a46ab6b89f71e
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-10 21:53:45 -0500

    Define `Decoder.decoded_frame()`

--- decode.py
+++ decode.py
@@ -34,6 +34,67 @@ class Decoder:
             pad_width(height, width, block_size)
         )
 
+    def decode_frame(
+        self,
+        reference_frames: np.ndarray,
+        motion_vectors: np.ndarray,
+        residuals: np.ndarray,
+    ) -> np.ndarray | None:
+        blocks_i: int
+        blocks_j: int
+        height: int
+        width: int
+        blocks_i, blocks_j, height, width = residuals.shape
+
+        block_size: int = self.config.stream.block_size
+
+        assert not height % block_size
+        assert not width % block_size
+
+        assert reference_frames.dtype == residuals.dtype
+
+        decoded_frame: np.ndarray = np.zeros(
+            shape=reference_frames.shape[-2:],
+            dtype=reference_frames.dtype,
+        )
+
+        def block2pel(x: int) -> int:
+            return x * block_size
+
+        def select_block(
+            i: int,
+            j: int,
+            motion_vector: np.ndarray,
+        ) -> tuple[int, slice, slice]:
+            reference_frame: int
+            x: int
+            y: int
+            reference_frame, y, x = motion_vector
+
+            y = block2pel(y + i)
+            x = block2pel(x + j)
+
+            block = (
+                reference_frame,
+                slice(y, y + block_size),
+                slice(x, x + block_size),
+            )
+
+            return block
+
+        for i in range(blocks_i):
+            for j in range(blocks_j):
+                motion_vector: np.ndarray = motion_vectors[i, j]
+                residual: np.ndarray = residuals[i, j]
+
+                block: tuple[int, slice, slice] = select_block(i, j, motion_vector)
+
+                reference_frame: np.ndarray = reference_frames[*block]
+
+                decoded_frame[*block[1:]] = reference_frame + residual
+
+        return decoded_frame
+
     @staticmethod
     def unpad_frame(
         pad_width: tuple[tuple[int, int], tuple[int, int]]
