a891520cbff5b8bed194aae51865a7cd5cebe110
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-30 22:19:40 -0500

    Define `Quantizer`

--- quantize.py
+++ quantize.py
@@ -2,3 +2,20 @@
 #
 # quantize.py -- quantize frames
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+
+import numpy as np
+
+from abc import (
+    ABC,
+    abstractmethod,
+)
+
+
+class Quantizer(ABC):
+    @abstractmethod
+    def quantize(self, x: np.ndarray) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def dequantize(self, x: np.ndarray) -> np.ndarray:
+        pass
