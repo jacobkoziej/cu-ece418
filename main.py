57172c7d3ff2cbaaaaf5b244f60717a6bb0c1205
Jacob Koziej <jacobkoziej@gmail.com>
2024-12-01 23:38:27 -0500

    Write encoded output to disk

--- main.py
+++ main.py
@@ -4,28 +4,52 @@
 # main.py -- video compressor
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
 
+import numpy as np
+
+from argparse import (
+    ArgumentParser,
+    Namespace,
+)
+from pathlib import Path
+from typing import (
+    Iterator,
+    Optional,
+)
+
+from tqdm import trange
+
+from encode import (
+    Encoder,
+    EncoderConfig,
+    EncoderFrameRateConfig,
+)
+from fetch import (
+    VideoMetadata,
+    frames_external_format,
+    probe_external_format,
+)
+from frame import (
+    FrameType,
+    StreamConfig,
+)
+from serialize import write
+
 
 def main():
-    from argparse import (
-        ArgumentParser,
-        Namespace,
-    )
-    from pathlib import Path
-    from sys import argv
-
-    parser = ArgumentParser(
-        prog=argv[0],
+    parser: ArgumentParser = ArgumentParser(
         description="video compressor",
     )
 
     parser.add_argument(
         "-i",
         "--input",
+        required=True,
         type=Path,
     )
     parser.add_argument(
         "-o",
         "--output",
+        required=True,
         type=Path,
     )
 
@@ -34,6 +58,35 @@ def main():
     args, ffmpeg_args = parser.parse_known_args()
 
     ffmpeg_args: dict[str, str] = dict(arg.split("=") for arg in ffmpeg_args)
+
+    metadata: VideoMetadata = probe_external_format(args.input, ffmpeg_args)
+    frame_fetcher: Iterator[np.ndarray] = frames_external_format(
+        args.input, metadata
+    )
+
+    stream_config: StreamConfig = StreamConfig()
+
+    frame_rate_config: EncoderFrameRateConfig = EncoderFrameRateConfig()
+    encoder_config: EncoderConfig = EncoderConfig(
+        frame_rate=frame_rate_config,
+        stream=stream_config,
+        metadata=metadata,
+    )
+
+    encoder: Encoder = Encoder(encoder_config, frame_fetcher)
+
+    with open(args.output, "wb") as f:
+        write(f, metadata)
+        write(f, stream_config)
+
+        for _ in trange(metadata.frames):
+            frames: Optional[list[FrameType]] = encoder.step(1)
+
+            if frames is None:
+                continue
+
+            for frame in frames:
+                write(f, frame)
 
 
 if __name__ == "__main__":
