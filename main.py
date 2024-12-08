c6dfe0bb5ffae357377ab72197873991b5ab917d
Jacob Koziej <jacobkoziej@gmail.com>
2024-12-08 15:19:21 -0500

    Only encode external formats

--- main.py
+++ main.py
@@ -35,6 +35,45 @@ from frame import (
 from serialize import write
 
 
+def encode(args: Namespace, ffmpeg_args: dict[str, str]):
+    metadata: VideoMetadata = probe_external_format(args.input, ffmpeg_args)
+    frame_fetcher: Iterator[np.ndarray] = frames_external_format(
+        args.input, metadata
+    )
+
+    stream_config: StreamConfig = StreamConfig(
+        block_size=args.block_size,
+        quality=args.quality,
+        reference_frames_max=args.reference_frames_max,
+    )
+
+    frame_rate_config: EncoderFrameRateConfig = EncoderFrameRateConfig(
+        i=args.i_frame_rate,
+        p=args.p_frame_rate,
+    )
+    encoder_config: EncoderConfig = EncoderConfig(
+        frame_rate=frame_rate_config,
+        stream=stream_config,
+        metadata=metadata,
+        search_limit=args.block_search_limit,
+    )
+
+    encoder: Encoder = Encoder(encoder_config, frame_fetcher)
+
+    with open(args.output, "wb") as f:
+        write(f, metadata)
+        write(f, stream_config)
+
+        for _ in trange(0, metadata.frames, args.step_size):
+            frames: Optional[list[FrameType]] = encoder.step(args.step_size)
+
+            if frames is None:
+                continue
+
+            for frame in frames:
+                write(f, frame)
+
+
 def main():
     parser: ArgumentParser = ArgumentParser(
         description="video compressor",
@@ -49,6 +88,12 @@ def main():
     parser.add_argument(
         "--block-size",
         default=16,
+        required=False,
+        type=int,
+    )
+    parser.add_argument(
+        "--decode-buffer-size",
+        default=32,
         required=False,
         type=int,
     )
@@ -102,42 +147,8 @@ def main():
 
     ffmpeg_args: dict[str, str] = dict(arg.split("=") for arg in ffmpeg_args)
 
-    metadata: VideoMetadata = probe_external_format(args.input, ffmpeg_args)
-    frame_fetcher: Iterator[np.ndarray] = frames_external_format(
-        args.input, metadata
-    )
-
-    stream_config: StreamConfig = StreamConfig(
-        block_size=args.block_size,
-        quality=args.quality,
-        reference_frames_max=args.reference_frames_max,
-    )
-
-    frame_rate_config: EncoderFrameRateConfig = EncoderFrameRateConfig(
-        i=args.i_frame_rate,
-        p=args.p_frame_rate,
-    )
-    encoder_config: EncoderConfig = EncoderConfig(
-        frame_rate=frame_rate_config,
-        stream=stream_config,
-        metadata=metadata,
-        search_limit=args.block_search_limit,
-    )
-
-    encoder: Encoder = Encoder(encoder_config, frame_fetcher)
-
-    with open(args.output, "wb") as f:
-        write(f, metadata)
-        write(f, stream_config)
-
-        for _ in trange(0, metadata.frames, args.step_size):
-            frames: Optional[list[FrameType]] = encoder.step(args.step_size)
-
-            if frames is None:
-                continue
-
-            for frame in frames:
-                write(f, frame)
+    if not args.input.suffix == ".cu-ece418":
+        encode(args, ffmpeg_args)
 
 
 if __name__ == "__main__":
